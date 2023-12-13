12b21fc931d0b6ba886c8cb85217a73cfc52e00a
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2023-12-14 02:41:23 +0900

    [Gold IV] Title: 작업, Time: 572 ms, Memory: 37616 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.py
+++ 백준/Gold/2056. 작업/작업.py
@@ -1,39 +1,35 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 from collections import deque
 
-N=int(input())
-in_degree=[0]*(N+1)
-dp=[0]*(N+1)
-ans=0
-time=[0]*(N+1)
-graph=[[] for _ in range(N+1)]
-q=deque()
+N = int(input())
+time = [0] * (N+1)
+time_dp = [0] * (N+1)
+degree = [0] * (N+1)
+now_work = deque()
+topology_graph = [[] for _ in range(N+1)]
 
 for i in range(1,N+1):
-    info=list(map(int,input().split()))
-    time[i]=info[0]
-    num=info[1]
-    if num==0:
-        continue
-    else:
-        in_degree[i] = info[1]
-        for j in range(2,2+num):
-            graph[info[j]].append(i)
+    jobs = list(map(int,input().split()))
+    time[i] = jobs[0]
+    time_dp[i] = jobs[0]
+
+    if jobs[1] > 0:
+        for j in range(2,len(jobs)):
+            degree[i]+=1
+            topology_graph[jobs[j]].append(i)
 
 for i in range(1,N+1):
-    if in_degree[i]==0:
-        dp[i]=time[i]
-        q.append(i)
+    if degree[i] == 0:
+        now_work.append(i)
 
-while q:
-    x=q.popleft()
+while now_work:
+    x = now_work.popleft()
 
-    for y in graph[x]:
-        if in_degree[y]>0:
-            dp[y]=max(dp[y],dp[x]+time[y])
-            in_degree[y]-=1
-        if not in_degree[y]:
-            q.append(y)
+    for y in topology_graph[x]:
+        degree[y] -= 1
+        time_dp[y] = max(time_dp[y],time_dp[x] + time[y])
+        if degree[y] == 0:
+            now_work.append(y)
 
-print(max(dp))
+print(max(time_dp))
