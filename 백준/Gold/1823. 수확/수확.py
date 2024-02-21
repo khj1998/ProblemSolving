76afc7d88e54755f817837e266b28cce006f2c2e
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2024-02-21 17:18:10 +0900

    [Gold III] Title: 수확, Time: 1372 ms, Memory: 343224 KB -BaekjoonHub

--- 백준/Gold/1823. 수확/수확.py
+++ 백준/Gold/1823. 수확/수확.py
@@ -1,25 +1,28 @@
 import sys
+sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
 
 N = int(input())
+array = []
 dp = [[0]*N for _ in range(N)]
-values = []
 
 for _ in range(N):
-    values.append(int(input()))
+    array.append(int(input()))
 
-def recursive(left,right,cnt):
-    if right < left:
-        return 0
-    elif dp[left][right]:
+for i in range(N):
+    dp[i][i] = array[i]
+
+def recursive_dp(left,right,cnt):
+    if left == right:
+        return dp[left][right]*cnt
+    if dp[left][right]:
         return dp[left][right]
 
-    dp[left][right] = max(cnt*values[left] + recursive(left+1,right,cnt+1),
-                          cnt*values[right] + recursive(left,right-1,cnt+1))
+    dp[left][right] = max(dp[left][left]*cnt + recursive_dp(left+1,right,cnt+1),
+                          dp[right][right]*cnt + recursive_dp(left,right-1,cnt+1))
 
     return dp[left][right]
 
-recursive(0,N-1,1)
+recursive_dp(0,N-1,1)
 
 print(dp[0][N-1])
