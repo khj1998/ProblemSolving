2559d8eb1508415ad2c61c0269df1dd0d5569e79
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2024-02-24 00:12:04 +0900

    [Gold V] Title: 치킨 배달, Time: 336 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,38 +1,41 @@
 import sys
+input = sys.stdin.readline
 from itertools import combinations
-from collections import deque
 
-def check_min(choice):
-    distance = 0
+#주어진 2의 치킨 거리를 각각 계산
+N,M = map(int,input().split())
+dx,dy = [-1,1,0,0],[0,0,-1,1]
+INF = int(1e8)
+ans = INF
+graph = []
+houses = []
+chicken_list = []
 
-    for house in houses:
-        house_dist = int(1e9)
-        for c in choice:
-            dist = abs(house[0]-c[0]) + abs(house[1]-c[1])
-            house_dist = min(house_dist,dist)
-        distance+=house_dist
-    return distance
+for _ in range(N):
+    graph.append(list(map(int,input().split())))
 
-if __name__ =="__main__":
-    N,M = map(int,input().split())
-    graph = []
-    houses = []
-    chickens = []
-    ans = int(1e9)
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 2:
+            chicken_list.append((i,j))
+        elif graph[i][j] == 1:
+            houses.append((i,j))
 
-    for _ in range(N):
-        graph.append(list(map(int,input().split())))
+combination = list(combinations(chicken_list,M))
 
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j] == 1:
-                houses.append((i,j))
-            elif graph[i][j] == 2:
-                chickens.append((i,j))
+def get_min_value(comb,houses):
+    result = 0
 
-    choices = list(combinations(chickens,M))
+    for x,y in houses:
+        temp = INF
 
-    for choice in choices:
-        ans = min(ans,check_min(choice))
+        for px,py in comb:
+            temp = min(temp,abs(x-px)+abs(y-py))
+        result += temp
 
-    print(ans)
+    return result
+
+for comb in combination:
+    ans = min(ans,get_min_value(comb,houses))
+
+print(ans)
