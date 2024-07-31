e436e829c46af7c83eaa310c97c55d80a250255a
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2024-07-31 22:20:52 +0900

    [Gold IV] Title: 연구소, Time: 2724 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,58 +1,58 @@
 import sys
 input = sys.stdin.readline
+from itertools import combinations
 from collections import deque
 from copy import deepcopy
-from itertools import combinations
 
+N,M = map(int,input().split())
+answer = -1
+virus_map = []
+wall_cases = []
 dx,dy = [-1,1,0,0],[0,0,-1,1]
 
-def spread(v_q):
-    ans = 0
+for _ in range(N):
+    virus_map.append(list(map(int,input().split())))
 
-    while v_q:
-        x,y = v_q.popleft()
+for i in range(N):
+    for j in range(M):
+        if virus_map[i][j] == 0:
+            wall_cases.append((i,j))
+
+wall_cases = list(combinations(wall_cases,3))
+
+def get_safe_area(case):
+    cnt = 0
+    q = deque()
+    temp_map = deepcopy(virus_map)
+
+    for x,y in case:
+        temp_map[x][y] = 1
+
+    for x in range(N):
+        for y in range(M):
+            if temp_map[x][y] == 2:
+                q.append((x,y))
+
+    while q:
+        x,y = q.popleft()
+
         for i in range(4):
-            px = x+dx[i]
-            py = y+dy[i]
+            px = x + dx[i]
+            py = y + dy[i]
 
             if 0<=px<N and 0<=py<M:
-                if temp_graph[px][py] == 0:
-                    temp_graph[px][py] = 1
-                    v_q.append((px,py))
+                if temp_map[px][py] == 0:
+                    temp_map[px][py] = 2
+                    q.append((px,py))
 
-    for i in range(N):
-        for j in range(M):
-            if temp_graph[i][j] == 0:
-                ans+=1
-    return ans
+    for x in range(N):
+        for y in range(M):
+            if temp_map[x][y] == 0:
+                cnt+=1
 
-if __name__ =="__main__":
-    N,M = map(int,input().split())
-    wall = []
-    graph = []
-    virsus = []
-    virus_q = deque()
-    answer = -1
+    return cnt
 
-    for _ in range(N):
-        graph.append(list(map(int,input().split())))
+for case in wall_cases:
+    answer = max(answer,get_safe_area(case))
 
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 0:
-                wall.append((i,j))
-            if graph[i][j] == 2:
-                virus_q.append((i,j))
-
-    walls = list(combinations(wall,3))
-
-    for w in walls:
-        temp_graph = deepcopy(graph)
-        temp_graph[w[0][0]][w[0][1]] = 1
-        temp_graph[w[1][0]][w[1][1]] = 1
-        temp_graph[w[2][0]][w[2][1]] = 1
-
-        temp_q = deepcopy(virus_q)
-        answer = max(answer,spread(temp_q))
-
-    print(answer)
+print(answer)
