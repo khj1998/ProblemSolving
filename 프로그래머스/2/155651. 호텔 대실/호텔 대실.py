105d8a5293be425d89d4c4e1ff5ca58db1b65513
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2025-08-02 11:42:57 +0900

    [level 2] Title: 호텔 대실, Time: 1.69 ms, Memory: 9.58 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,24 +1,28 @@
 import heapq
 
 def solution(book_time):
-    heap = []
-    book_times = []
+    answer = 0
+    real_book_time = []
+    h = []
     
-    for s,e in book_time:
-        s = int(s[:2])*60 + int(s[3:])
-        e = int(e[:2])*60 + int(e[3:])
-        book_times.append([s,e])
+    for start,end in book_time:
+        start_time = int(start[0:2])*60 + int(start[3:])
+        end_time = int(end[0:2])*60 + int(end[3:])
+        real_book_time.append((start_time,end_time))
+        
+    real_book_time.sort()
     
-    book_times.sort()
-    
-    for s,e in book_times:
-        if not heap:
-            heapq.heappush(heap,e)
+    for start_time,end_time in real_book_time:
+        if not h:
+            heapq.heappush(h,end_time)
             continue
         
-        if s >= heap[0]+10:
-            heapq.heappop(heap)
-            
-        heapq.heappush(heap,e)
+        end = heapq.heappop(h)
+        
+        if start_time < end + 10:
+            heapq.heappush(h,end_time)
+            heapq.heappush(h,end)
+        else:
+            heapq.heappush(h,end_time)
     
-    return len(heap)
+    return len(h)
