89d7f2602418b8269f710fcbf970f007d8507813
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2025-10-10 23:58:01 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 91.38 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,32 +1,45 @@
+# 각 숫자를 이진수로 표현하고, 빈 공간은 0으로 채워 넣는다.
+# 루트 노드는 0일 수가 없다. 루트가 0인데 자식이 있다면 이진트리로 만들 수 없다.
 def solution(numbers):
-    def check(num_bin,is_root_dummy):
-        if len(num_bin)==1:
-            return not is_root_dummy or num_bin[0]=="0"
+    answer = []
+    
+    def is_binary_tree(to_search_num):
+        if len(to_search_num) == 1:
+            return True
         
-        root_idx = len(num_bin)//2
-        root = num_bin[root_idx]
+        l = len(to_search_num)
+        mid_index = l//2
+        mid = to_search_num[mid_index]
         
-        if root=="1" and is_root_dummy:
+        left = to_search_num[:mid_index]
+        right = to_search_num[mid_index+1:]
+        
+        if mid == '0' and ('1' in left or '1' in right):
             return False
         
-        return check(num_bin[:root_idx],root=="0") and check(num_bin[root_idx+1:],root=="0")
-    
-    answer = []
-    node_num = 0
-    
-    for number in numbers:
-        node_num = 1
-        i = 1
+        result_1 = is_binary_tree(left)
+        result_2 = is_binary_tree(right)
         
-        num_bin = bin(number)[2:]
+        return result_1 and result_2
+
+    def get_max_len(number):
+        count = len(number)
+        hight = 1
         
-        while node_num < len(num_bin):
-            node_num += 2**i
-            i+=1
+        while True:
+            if 2**hight - 1 < count:
+                hight += 1
+                continue
+            break
             
-        num_bin = (node_num-len(num_bin))*'0' + num_bin
+        return 2**hight - 1
         
-        if check(num_bin,False):
+    for number in numbers:
+        num = bin(number)[2:]
+        max_len = get_max_len(num)
+        num = num.zfill(max_len)
+        
+        if is_binary_tree(num):
             answer.append(1)
         else:
             answer.append(0)
