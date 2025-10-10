35f133f5528a76147e511caa18a9e16c5ad9c3d6
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2025-10-11 00:55:43 +0900

    [level 3] Title: 표 병합, Time: 166.93 ms, Memory: 9.73 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.py
+++ 프로그래머스/3/150366. 표 병합/표 병합.py
@@ -1,79 +1,78 @@
-from collections import defaultdict
-
 def solution(commands):
+    merged = [[(r, c) for c in range(51)] for r in range(51)]
+    board = [["" for _ in range(51)] for _ in range(51)]
     answer = []
-    sheet = [['']*51 for _ in range(51)]
-    parent = [[] for _ in range(51)]
-    group = defaultdict(list)
-    
-    for i in range(51):
-        for j in range(51):
-            parent[i].append([i,j])
-            group[(i,j)] = [[i,j]]
 
-    def find_parent(x,y):
-        if parent[x][y] != [x,y]:
-            parent[x][y] = find_parent(parent[x][y][0],parent[x][y][1])
-        return parent[x][y]
-    
-    def union(x1,y1,x2,y2):
-        if x1==x2 and y1==y2:
+    def find(r, c):
+        if merged[r][c] == (r, c):
+            return r, c
+        pr, pc = merged[r][c]
+        merged[r][c] = find(pr, pc)
+        return merged[r][c]
+
+    def union(r1, c1, r2, c2):
+        pr1, pc1 = find(r1, c1)
+        pr2, pc2 = find(r2, c2)
+
+        if (pr1, pc1) == (pr2, pc2):
             return
-        x1,y1 = find_parent(x1,y1)
-        x2,y2 = find_parent(x2,y2)
-        value1,value2 = sheet[x1][y1],sheet[x2][y2]
-        
-        if x1 <= x2:
-            update(x1,y1,x2,y2,value1,value2)
+
+        if board[pr1][pc1] != "":
+            merged[pr2][pc2] = (pr1, pc1)
         else:
-            update(x2,y2,x1,y1,value1,value2)
-    
-    def update(px,py,x1,y1,value1,value2):
-        if value1 != "":
-            sheet[px][py] = value1
-        elif value2 != "":
-            sheet[px][py] = value2
+            merged[pr1][pc1] = (pr2, pc2)
+
+    for command in commands:
+        parts = command.split()
+        cmd = parts[0]
+
+        if cmd == "UPDATE":
+            if len(parts) == 4: # UPDATE r c value
+                r, c, value = int(parts[1]), int(parts[2]), parts[3]
+                pr, pc = find(r, c)
+                board[pr][pc] = value
+            else: # UPDATE value1 value2
+                value1, value2 = parts[1], parts[2]
+                for r in range(1, 51):
+                    for c in range(1, 51):
+                        pr, pc = find(r, c)
+                        if board[pr][pc] == value1:
+                            board[pr][pc] = value2
         
-        for x,y in group[(x1,y1)]:
-            parent[x][y] = [px,py]
-            group[(px,py)].append([x,y])
-    
-    for com in commands:
-        com = com.split()
-        
-        if com[0] == "UPDATE":
-            if len(com) == 4: # 자신이 속한 그룹이 있다면 모두 value로
-                r,c = parent[int(com[1])][int(com[2])]
-                sheet[r][c] = com[3]
-            else: # value1을 가지고 있는 셀 모두 update
-                for i in range(51):
-                    for j in range(51):
-                        r,c = parent[i][j]
-                        if sheet[r][c] == com[1]:
-                            sheet[r][c] = com[2]
-        elif com[0] == "MERGE":
-            r1,c1,r2,c2 = int(com[1]),int(com[2]),int(com[3]),int(com[4])
-            if find_parent(r1,c1) != find_parent(r2,c2):
-                union(r1,c1,r2,c2)
-        elif com[0] == "UNMERGE":
-            r,c = int(com[1]),int(com[2])
-            pr,pc = find_parent(r,c)
-            origin_val = sheet[pr][pc]
+        elif cmd == "MERGE":
+            r1, c1, r2, c2 = map(int, parts[1:])
+            # 자기 자신과 병합하는 경우는 무시
+            if (r1, c1) != (r2, c2):
+                union(r1, c1, r2, c2)
+
+        elif cmd == "UNMERGE":
+            r, c = map(int, parts[1:])
+            pr, pc = find(r, c)
+            # 부모 셀의 값을 기억
+            value_to_keep = board[pr][pc]
             
-            for x,y in group[(pr,pc)]:
-                if x==r and y==c:
-                    sheet[x][y] = origin_val
-                else:
-                    sheet[x][y] = ''
-                parent[x][y] = [x,y]
-                group[(x,y)] = [[x,y]]
-        elif com[0] == "PRINT": 
-            r,c = int(com[1]),int(com[2])
-            r,c = parent[r][c]
+            cells_to_unmerge = []
+            # 같은 그룹에 속한 모든 셀을 찾음
+            for i in range(1, 51):
+                for j in range(1, 51):
+                    if find(i, j) == (pr, pc):
+                        cells_to_unmerge.append((i, j))
             
-            if sheet[r][c] == '':
+            # 찾은 셀들의 병합을 해제하고 값을 초기화
+            for ur, uc in cells_to_unmerge:
+                merged[ur][uc] = (ur, uc)
+                board[ur][uc] = ""
+            
+            # 원래 (r, c) 셀에만 값을 다시 할당
+            board[r][c] = value_to_keep
+
+        elif cmd == "PRINT":
+            r, c = map(int, parts[1:])
+            pr, pc = find(r, c)
+            value = board[pr][pc]
+            if value == "":
                 answer.append("EMPTY")
             else:
-                answer.append(sheet[r][c])
-
+                answer.append(value)
+                
     return answer
